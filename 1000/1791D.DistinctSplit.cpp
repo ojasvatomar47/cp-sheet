ff478249afc694cc03c8b6e2305ecd578d092307
Ojasva Tomar <ojasvatomar47@gmail.com>
2024-03-08 09:37:28 +0530

    Problems solved on 07/03/2024

---
+++ 1000/1791D.DistinctSplit.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#define test(...)
+#define deb(...)
+#else
+#include "./deb.h"
+#endif
+
+#define ll long long int
+#define VEC vector<ll>
+#define MAT vector<vector<ll>>
+#define PLL pair<ll, ll>
+#define PCL pair<char, ll>
+
+ll gcd(ll x, ll y) { return y == 0 ? x : gcd(y, x % y); }
+ll lcm(ll x, ll y) { return x / __gcd(x, y) * y; }
+
+#define FAB(i, a, b) for (int i = a; i < b; ++i)
+#define RAB(i, a, b) for (int i = a; i > b; --i)
+#define FOR(i, n) FAB(i, 0, n)
+#define REV(i, n) RAB(i, n - 1, -1)
+#define RANGE(v, val) for (auto &val : v)
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i * i <= a; ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+template <typename T>
+void print(const T &x)
+{
+    cout << x << endl;
+}
+
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (const auto &val : v)
+        cout << val << " ";
+    cout << endl;
+}
+
+#define LINE(str) getline(cin, str, '\n')
+#define MIN(v) (*(min_element(v.begin(), v.end())))
+#define MAX(v) (*(max_element(v.begin(), v.end())))
+#define SUM(v) (accumulate(v.begin(), v.end(), 0ll))
+#define CEIL(x, y) ceil((double)x / (double)y)
+#define LOWER(v, x) lower_bound(v.begin(), v.end(), x) - v.begin()
+#define UPPER(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int MOD = 1e9 + 7;
+const int N = 1e7 + 5;
+
+void solve()
+{
+    ll n, maxi = -1;
+    cin >> n;
+    string str;
+    cin >> str;
+
+    VEC hsh1(26, 0), hsh2(26, 0);
+
+    FOR(i, n)
+    {
+        hsh1[str[i] - 'a']++;
+    }
+
+    FOR(i, n)
+    {
+        hsh2[str[i] - 'a']++, hsh1[str[i] - 'a']--;
+        ll x = 0, y = 0;
+
+        for (int i = 0; i < 26; i++)
+        {
+            if (hsh1[i])
+                x++;
+            if (hsh2[i])
+                y++;
+        }
+
+        maxi = max(maxi, x + y);
+    }
+    print(maxi);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t, testcase = 1;
+    cin >> t;
+    while (t--)
+    {
+        test(testcase++);
+        solve();
+    }
+}
---
+++ 1000/1849B.Monsters.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#define test(...)
+#define deb(...)
+#else
+#include "deb.h"
+#endif
+
+#define ll long long int
+#define VEC vector<ll>
+#define MAT vector<vector<ll>>
+#define PLL pair<ll, ll>
+#define PCL pair<char, ll>
+
+ll gcd(ll x, ll y) { return y == 0 ? x : gcd(y, x % y); }
+ll lcm(ll x, ll y) { return x / __gcd(x, y) * y; }
+
+#define FAB(i, a, b) for (int i = a; i < b; ++i)
+#define RAB(i, a, b) for (int i = a; i > b; --i)
+#define FOR(i, n) FAB(i, 0, n)
+#define REV(i, n) RAB(i, n - 1, -1)
+#define RANGE(v, val) for (auto &val : v)
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i * i <= a; ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+template <typename T>
+void print(const T &x)
+{
+    cout << x << endl;
+}
+
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (const auto &val : v)
+        cout << val << " ";
+    cout << endl;
+}
+
+#define LINE(str) getline(cin, str, '\n')
+#define MIN(v) (*(min_element(v.begin(), v.end())))
+#define MAX(v) (*(max_element(v.begin(), v.end())))
+#define SUM(v) (accumulate(v.begin(), v.end(), 0ll))
+#define CEIL(x, y) ceil((double)x / (double)y)
+#define LOWER(v, x) lower_bound(v.begin(), v.end(), x) - v.begin()
+#define UPPER(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int MOD = 1e9 + 7;
+const int N = 1e7 + 5;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    deb(n, k);
+    vector<PLL> vp(n);
+    FOR(i, n)
+    {
+        ll x;
+        cin >> x;
+        vp[i].first = x % k, vp[i].second = i;
+        if (vp[i].first == 0)
+            vp[i].first = k;
+    }
+    sort(vp.begin(), vp.end(), [](const pair<ll, ll> &a, const pair<ll, ll> &b)
+         { return (a.first != b.first) ? (a.first > b.first) : (b.second > a.second); });
+
+    deb(vp);
+    for (auto &it : vp)
+        cout << it.second + 1 << " ";
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t, testcase = 1;
+    cin >> t;
+    while (t--)
+    {
+        test(testcase++);
+        solve();
+    }
+}
