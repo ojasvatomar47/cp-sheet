53f522bc54120800ec16fa2caaa816f50d420367
Ojasva Tomar <ojasvatomar47@gmail.com>
2024-03-11 18:02:42 +0530

    Problems solved on 11/03/2024

---
+++ 1100/1511C.YetAnotherCardDeck.cpp
@@ -0,0 +1,112 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#define test(...)
+#define deb(...)
+#else
+#include "deb.h"
+#endif
+
+#define ll long long int
+#define VEC vector<ll>
+#define MAT vector<vector<ll>>
+#define PLL pair<ll, ll>
+#define PCL pair<char, ll>
+
+ll gcd(ll x, ll y) { return y == 0 ? x : gcd(y, x % y); }
+ll lcm(ll x, ll y) { return x / __gcd(x, y) * y; }
+
+#define FAB(i, a, b) for (int i = a; i < b; ++i)
+#define RAB(i, a, b) for (int i = a; i > b; --i)
+#define FOR(i, n) FAB(i, 0, n)
+#define REV(i, n) RAB(i, n - 1, -1)
+#define RANGE(v, val) for (auto &val : v)
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i * i <= a; ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+template <typename T>
+void print(const T &x)
+{
+    cout << x << endl;
+}
+
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (const auto &val : v)
+        cout << val << " ";
+    cout << endl;
+}
+
+#define LINE(str) getline(cin, str, '\n')
+#define MIN(v) (*(min_element(v.begin(), v.end())))
+#define MAX(v) (*(max_element(v.begin(), v.end())))
+#define SUM(v) (accumulate(v.begin(), v.end(), 0ll))
+#define CEIL(x, y) ceil((double)x / (double)y)
+#define LOWER(v, x) lower_bound(v.begin(), v.end(), x) - v.begin()
+#define UPPER(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int MOD = 1e9 + 7;
+const int N = 1e7 + 5;
+
+void solve()
+{
+    // since ai <= 50 [constraint], we can think of brute force here
+    ll n, q;
+    cin >> n >> q;
+    VEC hsh(51, 0), v, indices(51, 0);
+
+    FOR(i, n)
+    {
+        ll x;
+        cin >> x;
+        if (!hsh[x])
+            v.push_back(x), indices[x] = i + 1;
+        hsh[x]++;
+    }
+    n = v.size();
+    deb(v);
+
+    FOR(i, q)
+    {
+        ll x;
+        cin >> x;
+        cout << indices[x] << " ";
+
+        FOR(j, n)
+        {
+            // deb(v[j]);
+            if (indices[v[j]] < indices[x])
+                indices[v[j]]++;
+        }
+        indices[x] = 1;
+        deb(indices);
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t, testcase = 1;
+    cin >> t;
+    while (t--)
+    {
+        test(testcase++);
+        solve();
+    }
+}
---
+++ 1100/1618C.PaintTheArray.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+#define test(...)
+#define deb(...)
+#else
+#include "deb.h"
+#endif
+
+#define ll long long int
+#define VEC vector<ll>
+#define MAT vector<vector<ll>>
+#define PLL pair<ll, ll>
+#define PCL pair<char, ll>
+
+ll gcd(ll x, ll y) { return y == 0 ? x : gcd(y, x % y); }
+ll lcm(ll x, ll y) { return x / __gcd(x, y) * y; }
+
+#define FAB(i, a, b) for (int i = a; i < b; ++i)
+#define RAB(i, a, b) for (int i = a; i > b; --i)
+#define FOR(i, n) FAB(i, 0, n)
+#define REV(i, n) RAB(i, n - 1, -1)
+#define RANGE(v, val) for (auto &val : v)
+
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i * i <= a; ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+
+template <typename T>
+void print(const T &x)
+{
+    cout << x << endl;
+}
+
+template <typename T>
+void print(const vector<T> &v)
+{
+    for (const auto &val : v)
+        cout << val << " ";
+    cout << endl;
+}
+
+#define LINE(str) getline(cin, str, '\n')
+#define MIN(v) (*(min_element(v.begin(), v.end())))
+#define MAX(v) (*(max_element(v.begin(), v.end())))
+#define SUM(v) (accumulate(v.begin(), v.end(), 0ll))
+#define CEIL(x, y) ceil((double)x / (double)y)
+#define LOWER(v, x) lower_bound(v.begin(), v.end(), x) - v.begin()
+#define UPPER(v, x) upper_bound(v.begin(), v.end(), x) - v.begin()
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+const int MOD = 1e9 + 7;
+const int N = 1e7 + 5;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    VEC v(n);
+    FOR(i, n)
+        cin >> v[i];
+    ll g1 = v[0], g2 = v[1];
+
+    for (int i = 1; i < n; i += 2)
+        g2 = __gcd(g2, v[i]);
+    for (int i = 0; i < n; i += 2)
+        g1 = __gcd(g1, v[i]);
+
+    for (int i = 1; i < n; i += 2)
+    {
+        if (v[i] % g1 == 0)
+        {
+            g1 = -1;
+            break;
+        }
+    }
+    for (int i = 0; i < n; i += 2)
+    {
+        if (v[i] % g2 == 0)
+        {
+            g2 = -1;
+            break;
+        }
+    }
+
+    if (g1 == -1 && g2 == -1)
+        print(0);
+    else
+        print(max(g1, g2));
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    ll t, testcase = 1;
+    cin >> t;
+    while (t--)
+    {
+        test(testcase++);
+        solve();
+    }
+}
